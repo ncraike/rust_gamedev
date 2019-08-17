193f4970aa79ad4028dd551cfebf453d3e612987
Nathan Craike <ncraike@gmail.com>
2019-08-18 00:21:12 +1000

    Rewrites game to run in ggez window
    Player attributes are now shown with text in the `ggez` game window.
    
    Player actions are triggered by key presses instead of text prompt.
    
    Game immediately quits when the player quits or has no health.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,105 @@
-use std::io;
-use ggez::*;
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::conf;
+use ggez::event;
+use ggez::graphics;
+use ggez::input::keyboard;
+use ggez::nalgebra;
 
-struct State {
-    dt: std::time::Duration,
+
+fn main() {
+    let state = &mut GameState::new();
+    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello_ggez", "Nathan")
+        .conf(conf::Conf::new())
+        .build()
+        .unwrap();
+    event::run(ctx, event_loop, state).unwrap();
 }
 
-impl ggez::event::EventHandler for State {
+struct GameState {
+    player: Character,
+    status_text: StatusText,
+    action_prompt: graphics::Text,
+    pending_action: Option<UserAction>,
+}
+
+impl GameState {
+    fn new() -> GameState {
+        let mut new_game_state = GameState {
+            player: Character::new(),
+            status_text: StatusText::new(),
+            action_prompt: graphics::Text::new("Press E to eat food, W to wait, Q to quit"),
+            pending_action: None,
+        };
+        new_game_state.status_text.update_text(&new_game_state.player);
+        return new_game_state;
+    }
+}
+
+impl event::EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.dt = timer::delta(ctx);
+        match self.pending_action {
+            Some(UserAction::PlayerDo(player_action)) => {
+                self.player.do_action(player_action);
+                self.pending_action = None;
+                self.player.tick();
+                self.status_text.update_text(&self.player);
+            }
+            Some(UserAction::QuitGame) => {
+                event::quit(ctx);
+            }
+            _ => (),
+        }
+        if !self.player.is_alive() {
+            event::quit(ctx);
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+        graphics::draw(
+            ctx,
+            &self.status_text.health,
+            graphics::DrawParam::default().dest(nalgebra::Point2::new(0.0, 0.0)),
+        )?;
+        graphics::draw(
+            ctx,
+            &self.status_text.nutrition,
+            graphics::DrawParam::default().dest(nalgebra::Point2::new(0.0, 20.0)),
+        )?;
+        graphics::draw(
+            ctx,
+            &self.status_text.oxygen,
+            graphics::DrawParam::default().dest(nalgebra::Point2::new(0.0, 40.0)),
+        )?;
+        graphics::draw(
+            ctx,
+            &self.action_prompt,
+            graphics::DrawParam::default().dest(nalgebra::Point2::new(0.0, 80.0)),
+        )?;
+        graphics::present(ctx)?;
         Ok(())
     }
-}
 
-enum MyGameResult {
-    PlayerDeath,
-    UserQuits,
+    fn key_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: keyboard::KeyCode,
+        _keymods: keyboard::KeyMods,
+    ) {
+        match keycode {
+            keyboard::KeyCode::E => {
+                self.pending_action = Some(UserAction::PlayerDo(CharacterAction::Eat));
+            }
+            keyboard::KeyCode::W => {
+                self.pending_action = Some(UserAction::PlayerDo(CharacterAction::Wait));
+            }
+            keyboard::KeyCode::Q | keyboard::KeyCode::Escape => {
+                self.pending_action = Some(UserAction::QuitGame);
+            }
+            _ => (),
+        };
+    }
 }
 
 struct Character {
@@ -28,57 +108,20 @@ struct Character {
     oxygen: i64,
 }
 
-enum UserAction {
-    PlayerDo(CharacterAction),
-    QuitGame,
-}
-
-enum CharacterAction {
-    Eat,
-    Wait,
-}
-
-fn main() {
-    let state = &mut State { dt: std::time::Duration::new(0, 0) };
-    let my_conf = conf::Conf::new();
-    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello_ggez", "Nathan")
-        .conf(my_conf)
-        .build()
-        .unwrap();
-    event::run(ctx, event_loop, state).unwrap();
-}
-
-fn game_loop() -> MyGameResult {
-    let mut player = Character {
-        health: 10,
-        nutrition: 10,
-        oxygen: 10,
-    };
-
-    while player.is_alive() {
-        player.print_status();
-        let action = prompt_for_action();
-        match action {
-            UserAction::PlayerDo(player_action) => player.do_action(player_action),
-            UserAction::QuitGame => return MyGameResult::UserQuits,
+impl Character {
+    fn new() -> Character {
+        Character {
+            health: 5,
+            nutrition: 5,
+            oxygen: 5,
         }
-        player.tick();
     }
-    return MyGameResult::PlayerDeath;
 }
 
 impl Character {
 
     fn is_alive(&self) -> bool {
         self.health > 0
-    }
-
-    fn print_status(&self) {
-        println!("
-Health: {}
-Nutrition: {}
-Oxygen: {}
-", self.health, self.nutrition, self.oxygen);
     }
 
     fn do_action(&mut self, action: CharacterAction) {
@@ -99,22 +142,51 @@ Oxygen: {}
     }
 }
 
-fn prompt_for_action() -> UserAction {
-    loop {
-        println!("What do you do?
-E)at
-W)ait
-Q)uit the game
-");
-        let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input)
-            .expect("Failed to read line");
+#[derive(Copy,Clone)]
+enum UserAction {
+    PlayerDo(CharacterAction),
+    QuitGame,
+}
 
-        match user_input.chars().next() {
-            Some('e') => return UserAction::PlayerDo(CharacterAction::Eat),
-            Some('w') => return UserAction::PlayerDo(CharacterAction::Wait),
-            Some('q') => return UserAction::QuitGame,
-            _ => (),
-        };
+#[derive(Copy,Clone)]
+enum CharacterAction {
+    Eat,
+    Wait,
+}
+
+struct StatusText {
+    health: graphics::Text,
+    nutrition: graphics::Text,
+    oxygen: graphics::Text,
+}
+
+impl StatusText {
+    fn new() -> StatusText {
+        StatusText {
+            health: graphics::Text::new(""),
+            nutrition: graphics::Text::new(""),
+            oxygen: graphics::Text::new(""),
+        }
+    }
+
+    fn update_health(&mut self, health_value: i64) {
+        self.health = graphics::Text::new("Health: ");
+        self.health.add(health_value.to_string());
+    }
+
+    fn update_nutrition(&mut self, nutrition_value: i64) {
+        self.nutrition = graphics::Text::new("Nutrition: ");
+        self.nutrition.add(nutrition_value.to_string());
+    }
+
+    fn update_oxygen(&mut self, oxygen_value: i64) {
+        self.oxygen = graphics::Text::new("Oxygen: ");
+        self.oxygen.add(oxygen_value.to_string());
+    }
+
+    fn update_text(&mut self, player: &Character) {
+        self.update_health(player.health);
+        self.update_nutrition(player.nutrition);
+        self.update_oxygen(player.oxygen);
     }
 }
